214389bac956719231eab3ef502ce4b79840a346
Ay-afk-stack <pakhrin.ayush404@gmail.com>
2025-04-04 10:15:39 +0545

    added links,specific language,limit

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+topic = "techcrunch"
+
 api_key = os.getenv("API_KEY")
 
-url = f"https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey={api_key}"
+url = f"https://newsapi.org/v2/top-headlines?sources={topic}&apiKey={api_key}&language=en"
 
 # Make Request
 request  = requests.get(url)
@@ -18,12 +20,12 @@ news = request.json()
 # Access the articel titles and description
 articles = news['articles']
 
-body = """Subject:Todays News\n"""
+body = """Subject:Today's News\n"""
 
 # Writing data from the API to a text file
 for article in articles:
     if article['title'] is not None:
-        body = body + article['title']+"\n"+article['description']+"\n"+ f"Read more at {article['url']} {2*"\n"}"
+        body = body + article['title']+"\n"+article['description']+"\n"+ f"Read more at {article['url']} "+2*"\n"
 
 body = body.encode('UTF-8')
 send_email(body)
